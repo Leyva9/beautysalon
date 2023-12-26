4e971ea57c604ce428c9188be4e1a9a18db7476a
Leyva9 <leyva0919@gmail.com>
2023-12-26 09:15:14 -0500

    small changes

---
+++ controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+    namespace Controllers;
+    use MVC\Router;
+
+    class LoginController {
+
+        public static function login(Router $router) {
+            debugger("Desde login");
+            $router->render('auth/login.php', []);
+        }
+
+        public static function logout(Router $router) {
+            debugger("Desde logout");
+            $router->render('auth/login.php', []);
+        }
+
+        public static function register(Router $router) {
+            debugger("Desde register");
+            $router->render('auth/register.php', []);
+        }
+    }
