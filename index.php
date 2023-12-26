e121834c82cc2e469dfb0949cc093a9db6172cfd
Leyva9 <leyva0919@gmail.com>
2023-12-26 09:14:17 -0500

    Some Auth Routes Added

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 require_once './includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
+use Controllers\PagesController;
 
 $router = new Router();
 
+// index
+$router->get('/', [PagesController::class, 'index']);
+
+// login
+$router->get('/login', [LoginController::class, 'login']);
+$router->post('/login', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+$router->get('/register', [LoginController::class, 'register']);
+$router->post('/register', [LoginController::class, 'register']);
 
 
 
